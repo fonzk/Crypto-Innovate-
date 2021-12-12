86d3582cd9bc9384a33267541f2b319c54b9c571
fonzk <alfdiazr@hotmail.com>
2021-12-12 12:44:42 -0500

    dark mode part 1

---
+++ lib/ui/widgets/change__theme_buttib_widget.dart
@@ -0,0 +1,17 @@
+import 'package:cryptoinnovate/ui/themes/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ChangeThemeButtonWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    return Switch.adaptive(
+        value: themeProvider.isDarkMode,
+        onChanged: (value) {
+          final provider = Provider.of<ThemeProvider>(context, listen: false);
+          provider.toggleTheme(value);
+        });
+  }
+}
